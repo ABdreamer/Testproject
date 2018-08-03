7412c04375351b8e3f0e787b00e5d5626ab47554
Manjeet <Manjeet@acer-manjeetkumar>
2018-08-04 04:12:52 +0530

    test main class created

---
+++ Testproject/src/github_repo_test/git_Test.java
@@ -0,0 +1,12 @@
+package github_repo_test;
+
+public class git_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I have successfully created github repository");
+
+	}
+
+}
